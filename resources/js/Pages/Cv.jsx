f3b877cda767ee52dfe269959ae3586bb42eda52
nicolaydammer <nicolaydammer@gmail.com>
2023-10-27 10:32:11 +0200

    WIP: Make use of API endpoints

--- resources/js/Pages/Cv.jsx
+++ resources/js/Pages/Cv.jsx
@@ -1,7 +1,9 @@
+import HorizontalCardScrolling from "../components/HorizontalCardScrolling.jsx";
+
 export default function cv() {
     return <div className="container py-lg-5 h-100 d-flex flex-wrap align-items-md-center justify-content-center">
-        <div className={"col-md-11"}>
-            <p>test</p>
+        <div className={"scrollRootWrapper"}>
+            <HorizontalCardScrolling></HorizontalCardScrolling>
         </div>
     </div>
 }
---
+++ resources/js/components/HorizontalCardScrolling.jsx
@@ -0,0 +1,16 @@
+
+
+export default function HorizontalCardScrolling()
+{
+
+
+    return <div className={'scrollingWrapper'}>
+        <div className={'card'}><h2>card1</h2><p></p><p></p><p></p></div>
+        <div className={'card'}><h2>card2</h2><p></p><p></p><p></p></div>
+        <div className={'card'}><h2>card3</h2><p></p><p></p><p></p></div>
+        <div className={'card'}><h2>card4</h2><p></p><p></p><p></p></div>
+        <div className={'card'}><h2>card5</h2><p></p><p></p><p></p></div>
+        <div className={'card'}><h2>card5</h2><p></p><p></p><p></p></div>
+        <div className={'card'}><h2>card5</h2><p></p><p></p><p></p></div>
+    </div>
+}
